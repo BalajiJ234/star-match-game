c519efba8afe000fc76dfff03c1329882c0cc600
Balaji J <balajijanarthanan1997@gmail.com>
2021-07-05 09:15:06 +0530

    project setup - Star Match Game

---
+++ src/components/StarMatch.jsx
@@ -0,0 +1,31 @@
+// STAR MATCH - Starting Template
+
+import { utils } from "../helpers";
+
+const StarMatch = () => {
+  const stars = 6;
+  return (
+    <div className='game'>
+      <div className='help'>
+        Pick 1 or more numbers that sum to the number of stars
+      </div>
+      <div className='body'>
+        <div className='left'>
+          {utils.range(1, stars).map((starId) => (
+            <div key={starId} className='star' />
+          ))}
+        </div>
+        <div className='right'>
+          {utils.range(1, 9).map((number) => (
+            <button key={number} className='number'>
+              {number}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className='timer'>Time Remaining: 10</div>
+    </div>
+  );
+};
+
+export default StarMatch;
